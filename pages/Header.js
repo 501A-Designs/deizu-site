342a703ee7de7acba9cc708fae87d68eca8e8835
501A <art71903design.gmail.com>
2021-11-22 09:09:50 +0900

    updated website with gradient text

--- pages/Header.js
+++ pages/Header.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import styles from '../styles/Home.module.css'
+import { MdOutlineOpenInBrowser } from "react-icons/md";
+
 
 export default function Header() {
     return (
@@ -8,7 +10,7 @@ export default function Header() {
                 <img src="/deizuAppIconUpdated.png" />
             </a>
             <div className="alignItems">
-                <button className={styles.blueBtn} onClick={() => { window.open('https://deizu-site.firebaseapp.com/', "_blank") }}>アプリを開く</button>
+                <button className={styles.blueBtn} onClick={() => { window.open('https://deizu-site.firebaseapp.com/', "_blank") }}><MdOutlineOpenInBrowser className={styles.icon}/>アプリを開く</button>
             </div>
         </header>
     )
