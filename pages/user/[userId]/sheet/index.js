4d95e080469114b2c76d34f2459f0d54ad9f33fb
501A <art71903design@gmail.com>
2022-09-26 20:58:56 -0700

    added more hover effects

--- pages/user/[userId]/sheet/index.js
+++ pages/user/[userId]/sheet/index.js
@@ -17,7 +17,7 @@ import StaticScene from '../../../../lib/style/StaticScene'
 import { NextSeo } from 'next-seo';
 import { FiArrowLeft, FiArrowRightCircle, FiPlus } from 'react-icons/fi';
 import { useAutoAnimate } from '@formkit/auto-animate/react'
-import { InfinitySpin } from 'react-loader-spinner'
+import { InfinitySpin } from 'react-loader-spinner';
 
 export default function Index() {
     const router = useRouter();
